897ea625ed34586388bd0d576ede07aeed0f74c2
jefry <jefrydep@gmail.com>
2023-05-12 18:08:38 -0500

    drawable menu ready

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -1,6 +1,18 @@
 import { forwardRef, useState } from "react";
 import Link from "next/link";
-import { HomeIcon, CreditCardIcon, UserIcon, UsersIcon, UserGroupIcon, TableCellsIcon, HomeModernIcon, MapIcon } from "@heroicons/react/24/solid";
+import {
+  HomeIcon,
+  CreditCardIcon,
+  UserIcon,
+  UsersIcon,
+  UserGroupIcon,
+  TableCellsIcon,
+  HomeModernIcon,
+  MapIcon,
+  ChevronDownIcon,
+  ChevronUpDownIcon,
+  ChevronUpIcon,
+} from "@heroicons/react/24/solid";
 import { useRouter } from "next/router";
 
 const SideBar = forwardRef(({ showNav }: any, ref: any) => {
@@ -8,7 +20,10 @@ const SideBar = forwardRef(({ showNav }: any, ref: any) => {
   const router = useRouter();
 
   return (
-    <div ref={ref} className="fixed w-56 h-full scroll-smooth border bg-[#17286B]  shadow-sm">
+    <div
+      ref={ref}
+      className=" overflow-y-auto fixed w-56 h-screen scroll-smooth border bg-[#17286B]  shadow-sm"
+    >
       <div className="flex justify-center mt-6 mb-14">
         <picture className="bg-white rounded-full p-6">
           <img className="w-32 h-auto" src="/logo.png" alt="company logo" />
@@ -16,12 +31,12 @@ const SideBar = forwardRef(({ showNav }: any, ref: any) => {
         </picture>
       </div>
 
-      <div className="flex flex-col">
+      <div className="flex flex-col ">
         <Link href="/">
           <div
             className={`pl-6 py-3 mx-5 rounded text-center cursor-pointer mb-3 flex items-center transition-colors ${
               router.pathname == "/"
-              ? "bg-[#697DB3] text-white"
+                ? "bg-[#697DB3] text-white"
                 : "text-gray-400 hover:bg-[#3C4AB8] hover:text-white"
             }`}
           >
@@ -33,9 +48,9 @@ const SideBar = forwardRef(({ showNav }: any, ref: any) => {
             </div>
           </div>
         </Link>
-        <Link href="/usuarios">
+        <Link href="/usuarios" onClick={() => setShowMenu(!showMenu)}>
           <div
-            className={`pl-6 py-3 mx-5 rounded text-center cursor-pointer mb-3 flex items-center transition-colors ${
+            className={`pl-6 py-3 mx-5 rounded text-center cursor-pointer mb-3 gap-2 flex items-center transition-colors ${
               router.pathname == "/usuarios"
                 ? "bg-[#697DB3] text-white"
                 : "text-gray-400 hover:bg-[#3C4AB8] hover:text-white"
@@ -45,15 +60,24 @@ const SideBar = forwardRef(({ showNav }: any, ref: any) => {
               <UserIcon className="h-5 w-5" />
             </div>
             <div>
-              <p>Usuarios</p>
+              <p>Seguridad</p>
             </div>
+            {!showMenu ? (
+              <ChevronDownIcon className="h-5 w-5" />
+            ) : (
+              <ChevronUpIcon className="h-5 w-5" />
+            )}
           </div>
         </Link>
+        <div className=" ml-10 border-l-4 border-white rounded-sm">
+          <div className="pl-14 py-2 text-gray-400 ">Permisos</div>
+          <div className="pl-14 py-2  text-gray-400">Clientes</div>
+        </div>
         <Link href="/clientes">
           <div
             className={`pl-6 py-3 mx-5 rounded text-center cursor-pointer mb-3 flex items-center transition-colors ${
               router.pathname == "/clientes"
-              ? "bg-[#697DB3] text-white"
+                ? "bg-[#697DB3] text-white"
                 : "text-gray-400 hover:bg-[#3C4AB8] hover:text-white"
             }`}
           >
@@ -69,7 +93,7 @@ const SideBar = forwardRef(({ showNav }: any, ref: any) => {
           <div
             className={`pl-6 py-3 mx-5 rounded text-center cursor-pointer mb-3 flex items-center transition-colors ${
               router.pathname == "/ventas"
-              ? "bg-[#697DB3] text-white"
+                ? "bg-[#697DB3] text-white"
                 : "text-gray-400 hover:bg-[#3C4AB8] hover:text-white"
             }`}
           >
@@ -85,7 +109,7 @@ const SideBar = forwardRef(({ showNav }: any, ref: any) => {
           <div
             className={`pl-6 py-3 mx-5 rounded text-center cursor-pointer mb-3 flex items-center transition-colors ${
               router.pathname == "/proyectos"
-              ? "bg-[#697DB3] text-white"
+                ? "bg-[#697DB3] text-white"
                 : "text-gray-400 hover:bg-[#3C4AB8] hover:text-white"
             }`}
           >
@@ -101,7 +125,7 @@ const SideBar = forwardRef(({ showNav }: any, ref: any) => {
           <div
             className={`pl-6 py-3 mx-5 rounded text-center cursor-pointer mb-3 flex items-center transition-colors ${
               router.pathname == "/Lotes"
-              ? "bg-[#697DB3] text-white"
+                ? "bg-[#697DB3] text-white"
                 : "text-gray-400 hover:bg-[#3C4AB8] hover:text-white"
             }`}
           >
@@ -117,8 +141,8 @@ const SideBar = forwardRef(({ showNav }: any, ref: any) => {
           <div
             className={`pl-6 py-3 mx-5 rounded text-center cursor-pointer mb-3 flex items-center transition-colors ${
               router.pathname == "/manzana"
-              ? "bg-[#697DB3] text-white"
-              : "text-gray-400 hover:bg-[#3C4AB8] hover:text-white"
+                ? "bg-[#697DB3] text-white"
+                : "text-gray-400 hover:bg-[#3C4AB8] hover:text-white"
             }`}
           >
             <div className="mr-2">
@@ -133,7 +157,7 @@ const SideBar = forwardRef(({ showNav }: any, ref: any) => {
           <div
             className={`pl-6 py-3 mx-5 rounded text-center cursor-pointer mb-3 flex items-center transition-colors ${
               router.pathname == "/gastos"
-              ? "bg-[#697DB3] text-white"
+                ? "bg-[#697DB3] text-white"
                 : "text-gray-400 hover:bg-[#3C4AB8] hover:text-white"
             }`}
           >
@@ -152,8 +176,8 @@ const SideBar = forwardRef(({ showNav }: any, ref: any) => {
               onClick={() => setShowMenu(!showMenu)}
               className={`pl-6 py-3 mx-5 rounded text-center cursor-pointer  flex items-center transition-colors ${
                 router.pathname == "/reportes"
-                ? "bg-[#697DB3] text-white"
-                : "text-gray-400 hover:bg-[#3C4AB8] hover:text-white"
+                  ? "bg-[#697DB3] text-white"
+                  : "text-gray-400 hover:bg-[#3C4AB8] hover:text-white"
               }`}
             >
               <div className="mr-2">
@@ -176,7 +200,7 @@ const SideBar = forwardRef(({ showNav }: any, ref: any) => {
             </div>
           )}
         </div>
-      
+
         <div className="text-white  "> jefrydep@gmail.com</div>
       </div>
     </div>
