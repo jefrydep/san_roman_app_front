01e5b643ca3e94d11cde2c6da53b8cc46ab0983b
jefry <jefrydep@gmail.com>
2023-05-27 17:52:26 -0500

    oke evrything

--- pages/seguridad/usuarios/index.tsx
+++ pages/seguridad/usuarios/index.tsx
@@ -148,7 +148,7 @@ const usuarios = () => {
                         onClick={() => {
                           setShowModal(false);
                         }}
-                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                        className="bg-red-400 hover:bg-red-500 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
                         type="button"
                       >
                         Cancelar
